930b54fc49a6863e00fb6a6e4d7818c7bdd4027a
Ward, Sampson John <sampsondraw@gmail.com>
2020-07-01 15:36:10 +1200

    removed initial route

--- server.js
+++ server.js
@@ -10,12 +10,6 @@ connectDB();
 //  Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) =>
-  res.json({
-    msg: 'Welcome to the Contact Keeper API...',
-  })
-);
-
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
